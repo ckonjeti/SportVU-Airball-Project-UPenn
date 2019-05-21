a06c390335cb9315856ae4500ebe9adec2026a2d
ckonjeti1 <ckonjeti1@gmail.com>
2019-05-20 22:45:17 -0500

    gets player coordinates

--- file_parse.py
+++ file_parse.py
@@ -7,16 +7,32 @@ Created on Mon May 20 13:32:21 2019
 
 import json
 
+
+#opens game file
 with open ("game.json") as f:
     data = json.load(f)
     
+#enters event section of file
 events = data["events"]
 
-#prints the times
-for x in range (0, 453) :
-    players = events[x]['moments']
+#gets coordinates for specified player and appends to playerCoordinates list
+def get_coordinates(playerNum, arr):
+    temp = []
+    for x in range (0, len(events)) :
+        #print(x)
+        try:
+            players = events[x]['moments'][0][5]
+            for element in players:
+                temp.append(players[playerNum])
+            arr.append(temp)
+        except:
+            pass
     
-    for element in players:
-        print('time: ' + str(element[2]))
 
+
+playerCoordinates = []
+
+
+for i in range (0, len(events[0]['moments'][0][5])):
+    get_coordinates(i, playerCoordinates)
     
